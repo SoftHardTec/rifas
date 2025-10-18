bd43a97d82094cb034d1988ce061ac3de82392ed
Yonalfred-Dev <tecnologysofthard@gmail.com>
2025-10-18 12:05:13 -0400

    eliminar config add

--- src/components/raffle/Carousel/index.tsx
+++ src/components/raffle/Carousel/index.tsx
@@ -1,12 +1,17 @@
 import { Carousel } from "@mantine/carousel";
+import { useRef } from "react";
+import Autoplay from "embla-carousel-autoplay";
 
 export default function ImageCarousel() {
+  const autoplay = useRef(Autoplay({ delay: 2000, stopOnInteraction: false }));
   return (
     <Carousel
       loop
       withIndicators
       h="100%"
-      className="bg-gray-300 dark:bg-dark-400"
+      plugins={[autoplay.current]}
+      onMouseEnter={autoplay.current.stop}
+      onMouseLeave={autoplay.current.reset}
     >
       <Carousel.Slide>1</Carousel.Slide>
       <Carousel.Slide>2</Carousel.Slide>
