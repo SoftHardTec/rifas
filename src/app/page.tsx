75d3a9a9b028c702c1a69f20563fa7610b2b594f
Yonalfred-Dev <tecnologysofthard@gmail.com>
2025-10-28 15:36:20 -0400

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import { useState, useEffect } from "react";
 import { ColorSchemesSwitcher } from "@/components/ui/color-schemes-switcher";
 import { HomeRaffle } from "@/components/raffle/Home";
